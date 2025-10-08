20f7483571d39f144d9eb1d8b98439b30a9bb791
huyhuy1712 <anhhuy17122004@gmail.com>
2025-10-08 15:33:12 +0700

    huy

---
+++ MilkTeaApp/MilkTea.Cilent/Services/CTCongThucService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MilkTea.Client.Models;
+using System.Net.Http.Json;
+
+namespace MilkTea.Client.Services
+{
+    using System.Net.Http.Json;
+
+    public class CTCongThucService : ApiServiceBase
+    {
+        public async Task<List<ChiTietCongThuc>> GetChiTietCongThucTheoSPAsync(int maSP)
+        {
+            return await _http.GetChiTietCongThucTheoSPAsync<List<ChiTietCongThuc>>($"/api/chitietcongthuc/masp/{maSP}");
+        }
+    }
+
+}
