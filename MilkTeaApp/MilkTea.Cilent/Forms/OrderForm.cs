8a14dedf0045d89ff28ffcd550d10d4ac9b6bd04
Retsera <3122410309.sv.sgu.edu.vn>
2025-10-25 10:19:57 +0700

    them phan quyen cho them sua xoa don hang, them khuyen mai, them xoa excel nhap hang

--- MilkTeaApp/MilkTea.Cilent/Forms/OrderForm.cs
+++ MilkTeaApp/MilkTea.Cilent/Forms/OrderForm.cs
@@ -43,6 +43,8 @@ namespace MilkTea.Client.Forms
 
             //Bật tắt các nút theo quyền
             roundedButton1.Enabled = Session.HasPermission("Thêm sản phẩm");
+            btnXuatDon.Enabled = Session.HasPermission("Thêm đơn hàng");
+            roundedButton2.Enabled = Session.HasPermission("Thêm đơn hàng");
         }
 
         private async Task LoadLoaiAsync()
